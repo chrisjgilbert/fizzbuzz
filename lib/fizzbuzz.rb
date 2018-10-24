34762c588c54855e3e4b1df5bdd4821fe51d97b5
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-24 15:59:04 +0100

    refactored fizzbuzz_spec and fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 != 0
-    "fizz"
-  elsif num % 5 == 0 && num % 3 != 0
-    "buzz"
-  elsif num % 5 == 0 && num % 3 == 0
+  if num % 15 == 0
     "fizzbuzz"
+  elsif num % 5 == 0
+    "buzz"
+  elsif num % 3 == 0
+    "fizz"
   else
     num
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,17 +1,18 @@
 require "./lib/fizzbuzz"
 
 describe "Fizzbuzz" do
+  numbers = (1..100)
 
   it "returns 'fizz' when passed a multiple of 3" do
-    1.upto(100) do |num|
+    numbers.each do |num|
       if num % 3 == 0 && num % 5 != 0
-        expect(fizzbuzz(num)).to eq "fizz"
+        expect(fizzbuzz(num)).to eq("fizz")
       end
     end
   end
 
   it "returns 'buzz' when passed a multiple of 5" do
-    1.upto(100) do |num|
+    numbers.each do |num|
       if num % 5 == 0 && num % 3 != 0
         expect(fizzbuzz(num)).to eq "buzz"
       end
@@ -19,7 +20,7 @@ describe "Fizzbuzz" do
   end
 
   it "returns 'fizzbuzz' when passed a multiple of 5 and 3" do
-    1.upto(100) do |num|
+    numbers.each do |num|
       if num % 5 == 0 && num % 3 == 0
         expect(fizzbuzz(num)).to eq "fizzbuzz"
       end
@@ -27,7 +28,7 @@ describe "Fizzbuzz" do
   end
 
   it "returns the number when not a multiple of 5 or 3" do
-    1.upto(100) do |num|
+    numbers.each do |num|
       if num % 5 != 0 && num % 3 != 0
         expect(fizzbuzz(num)).to eq num
       end
