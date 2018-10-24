da3c3cbfafba42ac6b65f25d253a8e0aa86af17a
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-24 15:16:17 +0100

    test one refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,5 @@
-def fizzbuzz(number)
-  "fizz"
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 != 0
+    "fizz"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,12 @@
 require "./lib/fizzbuzz"
 
 describe "Fizzbuzz" do
-  it "returns 'fizz' when passed 3" do
-    expect(fizzbuzz(3)).to eq("fizz")
+
+  it "returns 'fizz' when passed a multiple of 3" do
+    1.upto(100) do |num|
+      if num % 3 == 0 && num % 5 != 0
+        expect(fizzbuzz(num)).to eq("fizz")
+      end
+    end
   end
 end
